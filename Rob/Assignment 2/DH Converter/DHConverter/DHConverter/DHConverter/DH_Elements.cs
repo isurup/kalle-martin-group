f12fcfd63633a2856d52d0e91404d9eac9e320c1
Hofendrior <Hofendrior@14e3fe60-ca12-11de-9133-ad767c7812c1>
2009-11-26 07:44:09 +0000

    Modified version (by me). Fixes (partly) parameter inclusion of alpha and a-variables, adds possibility of using prismatic joints (limited), dynamic arm-length between joints, probably other minor changes. Still very limited.

--- Rob/Assignment 2/DH Converter/DHConverter/DHConverter/DHConverter/DH_Elements.cs
+++ Rob/Assignment 2/DH Converter/DHConverter/DHConverter/DHConverter/DH_Elements.cs
@@ -11,19 +11,38 @@ namespace DHConverter
         public string Comment;        
         //---------------------               
         public Joint[] j = new Joint[10];//!!
-        public int jId;
+        public int jId = 0;
         //---------------------               
+
+        public Template()
+        {
+            this.j[0] = new Joint();
+        }
+
         public void AddJoint(Joint j)
         {
-            this.j[this.jId++] = j;            
+            this.jId++;
+            this.j[this.jId] = j;
+            this.j[this.jId+1] = new Joint();
         }
     }
     public class Joint
     {        
         public string joint;
+        public string type;
         public string a;
         public string alpha;
         public string d;
         public string theta;
+
+        public Joint()
+        {
+            joint = "0";
+            type = "nil";
+            a = "0";
+            alpha = "0";
+            d = "0";
+            theta = "0";
+        }
     }        
 }
