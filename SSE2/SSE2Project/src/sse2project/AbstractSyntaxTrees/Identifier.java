b26e95fd7f2cb96175cefe73a2b4f1018a82932e
kalle.grafstrom <kalle.grafstrom@14e3fe60-ca12-11de-9133-ad767c7812c1>
2010-02-22 19:15:11 +0000

    Unfinished AbstractSyntaxTrees package committed.

---
+++ SSE2/SSE2Project/src/sse2project/AbstractSyntaxTrees/Identifier.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sse2project.AbstractSyntaxTrees;
+
+/**
+ *
+ * @author Kalle
+ */
+import sse2project.SourcePosition;
+
+public class Identifier extends Terminal {
+
+  public Identifier (String theSpelling, SourcePosition thePosition) {
+    super (theSpelling, thePosition);
+    //type = null;
+    //decl = null;
+  }
+
+  //public Object visit(Visitor v, Object o) {
+    //return v.visitIdentifier(this, o);
+  //}
+
+  //public TypeDenoter type;
+  //public AST decl; // Either a Declaration or a FieldTypeDenoter
+}
