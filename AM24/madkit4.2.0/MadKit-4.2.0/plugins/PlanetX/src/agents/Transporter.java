29dbace9569bd9b08318c88d9a8ff22c012742c8
morten00knudsen <morten00knudsen@14e3fe60-ca12-11de-9133-ad767c7812c1>
2010-08-07 13:41:28 +0000

    Added: SpaceShip.java Fixed: setup in Explorer.java and Transporter.java

--- AM24/madkit4.2.0/MadKit-4.2.0/plugins/PlanetX/src/agents/Transporter.java
+++ AM24/madkit4.2.0/MadKit-4.2.0/plugins/PlanetX/src/agents/Transporter.java
@@ -1,7 +1,32 @@
 package agents;
 
+import java.awt.Color;
+
 import turtlekit.kernel.Turtle;
 
 public class Transporter extends Turtle {
+	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Transporter() {
+		super();
+	}
+	
+	public void setup()
+	{
+		playRole("Transporter");
+		setColor(Color.CYAN);
+		setHeading(NorthEast);
+	}
+	
+	void wiggle() {
+		fd(1);
+		turnRight(Math.random() * 45);
+		turnLeft(Math.random() * 45);
+	}
 
 }
