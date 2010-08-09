91c58091c1bc8aa6d34debb9d49a6dcc24ee2a50
morten00knudsen <morten00knudsen@14e3fe60-ca12-11de-9133-ad767c7812c1>
2010-08-09 11:36:08 +0000

    Added: New functional projekt

---
+++ AM24/madkit4.2.0/MadKit-4.2.0/plugins/AM24/src/am24/agents/AM24Explorer.java
@@ -0,0 +1,64 @@
+/*
+* AM24Turtle.java -TurtleKit - A 'star logo' in MadKit
+* Copyright (C) 2000-2002 Fabien Michel
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+package am24.agents;
+
+import java.awt.Color;
+import turtlekit.kernel.Turtle;
+
+ public class AM24Explorer extends Turtle 
+ {
+	 int count=10;
+	 
+	 public AM24Explorer(){
+	 	super();
+	 }
+	 
+	 public AM24Explorer(String s)
+	  {super(s);}
+	
+	 public void setup(){
+	  	randomHeading();
+	  	setColor(Color.WHITE);
+	  	playRole("explorer");
+	  }
+	
+	 public String walk(){
+		fd(1);
+	   	if (count < 0) {
+	      count = (int) (Math.random()*90);
+	      return("change");
+	    }
+	   	else {
+	  	  count--;
+	 	  return ("walk");
+	   	}
+	}  
+	
+	 public String change(){
+		  randomHeading();
+		  /*if (getHeading() > South) setColor(Color.red);
+		  else if (getHeading() > West) setColor(Color.blue);
+		    else if (getHeading() > North) setColor(Color.green);
+		      else setColor(Color.yellow);*/
+		  return("walk");
+  }
+
+}
+
+
---
+++ AM24/madkit4.2.0/MadKit-4.2.0/plugins/AM24/src/am24/util/PlanetConstraints.java
@@ -0,0 +1,76 @@
+package am24.util;
+
+public class PlanetConstraints {
+	
+	/***
+	 * RobotEnergy - the amount of energy a robot has
+	 */
+	public static final int robotEnergy = 5000;
+	
+	/***
+	 * nbOfBases - Number of bases.
+	 */
+	public static final int nbOfBases = 2;
+	
+	/***
+	 * oreDensity - Density of ore.
+	 */
+	public static final double oreDensity = 0.05;
+	
+	
+	/***
+	 * gridSizeHeight - The Height;
+	 */
+	public static final int gridSizeHeight = 200;
+	
+
+	/***
+	 * gridSizeWidth - The Width;
+	 */
+	public static final int gridSizeWidth = 200;
+	
+	/**
+	 * mode - Coordination mode between bases,cooperative = 1 or competitive = 0.
+	 */
+	public static final int mode = 1;
+	
+	/*** 
+	 * nbOfExplorers - Number of explorers per base.
+	 */
+	public static final int nbOfExplorers = 10;
+
+	/***
+	 * nbOfTransporter - Number of transporters per base.
+	 */
+	public static final int nbOfTransporter = 10;
+	
+	/***
+	 * robotPerceptionScope
+	 */
+	public static final int robotPerceptionScope = 5;
+	
+	/***
+	 * I - fixed communication scope.
+	 */
+	public static final int fixedRobotCommunicationScope = 10;
+	
+	/***
+	 * robotMemorySize
+	 */
+	public static final int robotMemorySize = 10;
+	
+	/***
+	 * C - Capacity of each base in number of ore samples.
+	 */
+	public static final int baseCapacity = 200;
+	
+	/***
+	 * W - Maximal number of ore a robot can grab and/or carry.
+	 */
+	public static final int transporterOreCapacity = 8;
+	
+	/***
+	 * maxSimulationTime  - Maximal number of cycles.
+	 */
+	public static final int maxSimulationTime = 10000;
+}
